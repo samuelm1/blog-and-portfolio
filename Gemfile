7b112c0804caa4372cddf98f59208d7301921d9d
sirme <samgathitho@gmail.com>
2019-08-04 16:36:48 +0300

    Added will_paginate gem to paginate posts

--- Gemfile
+++ Gemfile
@@ -37,6 +37,7 @@ gem 'bootsnap', '1.4.1', require: false
 gem 'redcarpet'
 gem 'pygments.rb'
 gem 'friendly_id'
+gem 'will_paginate'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,12 @@
 class PostsController < ApplicationController
 	before_action :find_post,only:[:show, :edit,:update, :destroy]
 	def index
-		
+		@post = Post.all.order("created_at desc").paginate(page: params[:page], per_page: 1)
 	end
 	def new
 		@post = Post.new
 	end
-	def create
+	def create 
 		@post = Post.new(post_params)
 
 		if @post.save
