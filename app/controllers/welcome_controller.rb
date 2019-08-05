8419242f542a4abe0cf5264901a1c8b21299455c
sirme <samgathitho@gmail.com>
2019-08-05 09:17:41 +0300

    setup and styled basic projects functionality

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,6 @@
 class WelcomeController < ApplicationController
 	def index
 		@post = Post.all.limit(3).order("created_at desc")
+		@project = Project.all.limit(3).order("created_at desc")
 	end
 end
