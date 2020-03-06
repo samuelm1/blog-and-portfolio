e128664e3c3abc0eca4709a6c3a60a5f38e3e0b2
sirme <samgathitho@gmail.com>
2020-03-06 07:07:12 +0300

    Setup contact form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	resources :posts
 	resources :projects
+	resources :contacts, only: [:new, :create]
 	get 'welcome/index'
 	root "welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
